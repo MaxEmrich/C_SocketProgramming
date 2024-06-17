e304940090b3e90a325d8eec3007b88c22d53ca9
Max Emrich <maxfisherbear@gmail.com>
2024-06-17 13:29:58 -0700

    fixed file reading for the http server html now renders in multi-line html files

--- http_implementation/http_server.c
+++ http_implementation/http_server.c
@@ -11,16 +11,47 @@ const int MY_PORT = 60000;
 
 int main() {
 
-    // open a file to serve 
+    printf("starting program...\n");
+    fflush(stdout); // Flush the output buffer... flush means to force all text print immediately 
+
+    // open a file to serve and read it to a buffer
     FILE* html_data;
-    html_data = fopen("index.html", "r"); // index.html is the response body, "r" means reading from file
+    html_data = fopen("./index.html", "r"); // index.html is the response body, "r" means reading from file
+
+    if (html_data == NULL) {
+        printf("the file didn't open\n");
+        fflush(stdout); 
+    } else {
+        printf("the file opened\n");
+        fflush(stdout); 
+    }
 
     char response_data[1024];
-    fgets(response_data, 1024, html_data); // (where you're reading to, how much space are you reading to, where you're reading from)
+
+    char* source = NULL; 
+
+    if (fseek(html_data, 0L, SEEK_END) == 0) { // move the pointer to the end of the file, returns 0 if successful, hence the if-statement
+        long buff_size = ftell(html_data); // ftell tells us how long the file is IF the pointer is at the end of the file 
+        source = malloc(sizeof(buff_size + 1));
+
+        if (fseek(html_data, 0L, SEEK_SET) == 0) {
+            size_t newLen = fread(source, sizeof(char), buff_size, html_data); // fread returns a size_t, which is the num of elements that are read succesfully from the file
+            // fread() writes the memoru from the file into the buffer, in this case "source"
+        }
+    }
+
+    fclose(html_data);
+
+    
+
+    // fgets(response_data, 1024, html_data); // (where you're reading to, how much space are you reading to, where you're reading from)
+
+    // ----------------------------------------------------
 
     char http_header[2048] = "HTTP/1.1 200 OK\r\n\n"; // the response header that lets the client know that the response is OK and there are no errors
+    // char different_header[1023] = "HTTP/3.0 401 Unauthorized\r\n\n";
 
-    strcat(http_header, response_data); // adding data from response_data INTO in the http_header
+    strcat(http_header, source); // adding data from response_data INTO in the http_header
 
     // creeate a socket
     int server_socket;
@@ -29,7 +60,7 @@ int main() {
     // define the address
     struct sockaddr_in server_address;
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(MY_PORT);
+    server_address.sin_port = htons(MY_PORT); 
     server_address.sin_addr.s_addr = INADDR_ANY;
 
     bind(server_socket, (struct sockaddr*) &server_address, sizeof(server_address));
@@ -41,8 +72,12 @@ int main() {
     while (1) {
         client_socket = accept(server_socket, NULL, NULL);
         send(client_socket, http_header, sizeof(http_header), 0);
+        // send(client_socket, different_header, sizeof(different_header), 0);
         close(client_socket);
     }
+
+    free(source);
+
     return 0;
 }
 
